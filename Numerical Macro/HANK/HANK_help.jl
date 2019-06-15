e848203a409b644533b276025b3374688412514a
Daniele Caratelli <danicaratelli@gmail.com>
2019-06-15 14:53:45 -0700

    corrected r vs R in solve_HANK + starting stationary distribution business (it's not easy to get it to be fast)

--- Numerical Macro/HANK/HANK_help.jl
+++ Numerical Macro/HANK/HANK_help.jl
@@ -3,7 +3,7 @@
     author:         Daniele Caratelli (danicaratelli@gmail.com)
 =#
 
-function solveHANK(K0::Number,tau0::Number,Tran::Array{Float64,2},params:Dict)
+function solveHANK(K0::Number,tau0::Number,Tran::Array{Float64,2},params::Dict)
 
     #loading parameter values
     β = params["β"];
@@ -20,16 +20,16 @@ function solveHANK(K0::Number,tau0::Number,Tran::Array{Float64,2},params:Dict)
         #compute the wage w and the interest rate r based on guesses for capital
         #stock K
     w = (1-α)*(K0/L)^(α);
-    R = α*(N/K0)^(1-α) - δ;
+    r = α*(L/K0)^(1-α) - δ;
 
     #step 3:
         #solving HH's problem
-    cpol, apol = HHegm(params,tau0,w,R-1,Tran);
+    @time cpol, apol = HHegm(params,tau0,w,r,Tran);
 
     #step 4:
         #computing the stationary distribution of assets via simulation
-
-
+    #defining a new finer grid
+    Fdist = ones(NN,2)./(2*NN);
 end
 
 
@@ -54,6 +54,7 @@ function HHegm(params::Dict,τ::Number,w::Number,r::Number,Tran::Array{Float64,2
     δ = params["δ"];
     η = params["η"];
     N = params["N"];
+    b = params["b"];
     a_grid = params["a_grid"];
 
     dist = Inf;
@@ -61,15 +62,16 @@ function HHegm(params::Dict,τ::Number,w::Number,r::Number,Tran::Array{Float64,2
     cpol_old = ones(N,2);
     cpol_new = ones(N,2);
     while dist>tol && iter<maxiter
-        cpol_tmp = (β*cpol_old.^(-η)*(1+(1-τ)*r)*Tran').^(-1/η);
+        cpol_tmp = (β*(1+(1-τ)*r)*cpol_old.^(-η)*Tran').^(-1/η);
 
         #assets in current period
-        a_tmp = (a_grid .+ cpol_tmp .- [(1-τ)*w b])./(1+(1-τ)*r);
+        a_tmp = (a_grid .+ cpol_tmp .- [b (1-τ)*w])./(1+(1-τ)*r);
+
         for m=1:2
 
             #binding constraint
             loc_constrained = findall(a_grid.<=a_tmp[1,m]);
-            cpol_new[loc_constrained,m] = a_grid[loc_constrained]*(1+(1-τ)*r) .+ (abs(m-2)*(1-τ)*w .+ b*(m-1)) .- a_grid[1];
+            cpol_new[loc_constrained,m] = a_grid[loc_constrained]*(1+(1-τ)*r) .+ (b*abs(m-2) + (m-1)*(1-τ)*w) .- a_grid[1];
 
             #non-binding constraint
             cgrid_end = findall(a_grid.>a_tmp[end,m]);
@@ -82,6 +84,7 @@ function HHegm(params::Dict,τ::Number,w::Number,r::Number,Tran::Array{Float64,2
             cpol_new[cgrid_end,m] .= itp(a_tmp[end,m]);
         end
 
+
         dist = maximum(abs.(cpol_new.-cpol_old)[:]);
         if iter%100==0
             println("Iteration #"*string(iter)*", dif = "*string(dist));
@@ -91,13 +94,91 @@ function HHegm(params::Dict,τ::Number,w::Number,r::Number,Tran::Array{Float64,2
     end
 
     #imputing savings policy function
-    apol_new = a_grid.*(1+(1-τ)*r) .+ [(1-τ)*w b] .- cpol_new;
+    apol_new = a_grid.*(1+(1-τ)*r) .+ [b (1-τ)*w] .- cpol_new;
     println("Ending EGM.");
     println("\n");
     return cpol_new, apol_new;
 end
 
 
-function simul_dist()
+
+
+function simul_dist(apol,N,a_grid)
+
+    #finding the policy for assets in index form
+    apol_idx = Int.(zeros(apol));
+    #first finding apol_idx for those at constraint
+    locsi = findall(apol.==a_grid[1]);
+    apol_idx[locsi] .= 1;
+
+    for m=1:2
+        if m==1
+            st = locsi[end][1] + 1;
+        else
+            st = 1;
+        end
+        for n=st:N
+            locsi = findlast(apol[n,m].>=a_grid)
+            apol_idx[n,m] = locsi;
+        end
+    end
+
+    K = 1000;
+    eps_rand = rand(K,2);
+    Fdist = ones(NN,2)./(2*NN);
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function invapol(apol,a_grid)
+    amin = a_grid[1]; #borrowing limit
+
+    #constructing finer version of grid
+    a_grid_mid = mean([a_grid[2:end] a_grid[1:end-1]],dims=2);
+    a_grid_fine = sort([a_grid;a_grid_mid],dims=1);
+    N = length(a_grid_fine);
+
+    apol_inv = zeros(N,2);
+    #filling in for those at borrowing constraint
+    last_con = findlast(apol[:,m].<=amin);
+    if !isempty(last_con)
+        apol_inv[1,m] = a_grid[last_con];
+    else
+        last_con = 0;
+    end
+
+    #interpolating over monotone region
+    for n=2:end
+        ll = findlast(apol[:,m].<=a_grid_fine[n]);
+        apol_inv[];
+    end
+
+
+    knots = (apol[last_con+1:end,m],);
+    itp = interpolate(knots, a_grid[last_con+1:end], Gridded(Linear()));
+    apol_max = apol[end,m];
+    monotone_locs = findall((a_grid_fine.>apol[last_con+1,m]) .* (a_grid_fine[:,m].<=apol_max));
+    itp1 = itp.(a_grid_fine[monotone_locs]);
+
+    apol_inv[]
+    itp_new = interpolate()
+    itp2 = itp(itp1,)
 
 end
--- Numerical Macro/HANK/HANK_main.jl
+++ Numerical Macro/HANK/HANK_main.jl
@@ -3,4 +3,41 @@ filename:       HANK_main.jl
 description:    Replicated model in Chapter 7.1 of Herr and Maussner, a simple
                 heterogeneous agent model. It solves for the steady state
                 (distributions and aggregates) in general equilibrium.
-author:         Daniele Caratelli (danicaratelli@gmail.com)                
+author:         Daniele Caratelli (danicaratelli@gmail.com)
+=#
+
+#*************** PACKAGES ***************#
+using QuantEcon, Interpolations, Distributions, PyPlot
+pygui(true)
+include("HANK_help.jl")
+
+
+#*************** INPUTS ***************#
+
+#Employment-Unemployment transition matrix
+puu = 0.5000; pue = 0.5000;
+peu = 0.0435; pee = 0.9565
+Tran = [puu pue;
+        peu pee];
+
+#Initial guess for capital stock K and tax rate tau
+K0 = 5;
+tau0 = 0.3;
+
+#other parameters:
+params = Dict();
+params["β"] = 0.98;     #discount factor
+params["α"] = 1/3;      #capital share
+params["δ"] = 0.025;    #depreciation rate
+params["η"] = 2;        #risk aversion parameter
+params["b"] = 0.5;      #home production
+
+#Asset grid
+N = 10000;
+params["N"] = N;
+amin = -2;
+amax = 3000;
+a_grid = exp.(collect(range(log(1e-4),stop=log(amax-amin),length=N))) + amin;
+params["a_grid"] = a_grid;
+
+K0 = 250; tau0 = 0.3;

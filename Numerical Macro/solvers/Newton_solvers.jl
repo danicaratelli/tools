bd915916e1be6ca19b4fe0c74c62bfaa9cd4a022
Daniele Caratelli <danicaratelli@gmail.com>
2019-09-18 19:20:24 -0700

    working on Broyden's method

--- Numerical Macro/solvers/Newton_solvers.jl
+++ Numerical Macro/solvers/Newton_solvers.jl
@@ -1,66 +1,94 @@
-function NewtonRaphson(f,fprime,xstart,Xs,tol=1e-8,maxiter=10000)
-    fnew(x) = x - f(x)/fprime(x);
+function NewtonRaphson(f, fprime, xstart, Xs, tol = 1e-8, maxiter = 10000)
+    fnew(x) = x - f(x) / fprime(x)
 
-    dd = Inf;
-    it = 0;
-    xloop = copy(xstart);
-    @assert ((xloop>=Xs[1]) && (xloop<=Xs[end])) "Starting value is not in function support Xs"
-    while (abs(f(xloop))>tol) && (it<maxiter)
-        xloop = fnew(xloop);
-        if xloop<Xs[1]
-            xloop = Xs[1];
-        elseif xloop>Xs[end]
-            xloop = Xs[end];
+    dd = Inf
+    it = 0
+    xloop = copy(xstart)
+    @assert ((xloop >= Xs[1]) && (xloop <= Xs[end])) "Starting value is not in function support Xs"
+    while (abs(f(xloop)) > tol) && (it < maxiter)
+        xloop = fnew(xloop)
+        if xloop < Xs[1]
+            xloop = Xs[1]
+        elseif xloop > Xs[end]
+            xloop = Xs[end]
         end
-        it+=1;
+        it += 1
     end
-    return xloop;
+    return xloop
 end
 
 
-function NewtonRaphson_secant(f,x0,x1,Xs,tol=1e-8,maxiter=10000)
+function NewtonRaphson_secant(f, x0, x1, Xs, tol = 1e-8, maxiter = 10000)
 
-    dd = Inf;
-    it = 0;
-    @assert ((x0>=Xs[1]) && (x0<=Xs[end]) && (x1>=Xs[1]) && (x1<=Xs[end])) "Starting value is not in function support Xs"
-    while (abs(f(x1))>tol) && (it<maxiter)
-        xnew = x1  - f(x1)*(x1 - x0)/(f(x1)-f(x0));
-        if xnew<Xs[1]
-            xnew = Xs[1];
-        elseif xnew>Xs[end]
-            xnew = Xs[end];
+    dd = Inf
+    it = 0
+    @assert ((x0 >= Xs[1]) &&
+             (x0 <= Xs[end]) && (x1 >= Xs[1]) && (x1 <= Xs[end])) "Starting value is not in function support Xs"
+    while (abs(f(x1)) > tol) && (it < maxiter)
+        xnew = x1 - f(x1) * (x1 - x0) / (f(x1) - f(x0))
+        if xnew < Xs[1]
+            xnew = Xs[1]
+        elseif xnew > Xs[end]
+            xnew = Xs[end]
         end
-        x0 = copy(x1);
-        x1 = copy(xnew);
-        it+=1;
+        x0 = copy(x1)
+        x1 = copy(xnew)
+        it += 1
     end
-    println(it)
-    return x1;
+    return x1
 end
 
 
-function NewtonRaphson_MV(f,J,xstart,Xs,tol=1e-8,maxiter=10000)
-    fnew(x) = x - inv(J(x))*f(x);
+function NewtonRaphson_MV(f, J, xstart, Xs, tol = 1e-8, maxiter = 10000)
+    fnew(x) = x - inv(J(x)) * f(x)
 
-    dd = Inf;
-    it = 0;
-    ndim = length(xstart);
-    xloop = copy(xstart);
+    dd = Inf
+    it = 0
+    ndim = length(xstart)
+    xloop = copy(xstart)
     #making sure starting x is in support
-    for n=1:ndim
-        @assert ((xloop[n]>=Xs[n,1]) && (xloop[n]<=Xs[n,end])) "Starting value is not in function support Xs"
+    for n = 1:ndim
+        @assert ((xloop[n] >= Xs[n, 1]) && (xloop[n] <= Xs[n, end])) "Starting value is not in function support Xs"
     end
-    while (sum(abs.(f(xloop)))>tol) && (it<maxiter)
-        xloop = fnew(xloop);
-        for n=1:ndim
-            if xloop[n]<Xs[n,1]
-                xloop[n] = Xs[n,1];
-            elseif xloop[n]>Xs[n,end]
-                xloop[n] = Xs[n,end];
+    while (sum(abs.(f(xloop))) > tol) && (it < maxiter)
+        xloop = fnew(xloop)
+        for n = 1:ndim
+            if xloop[n] < Xs[n, 1]
+                xloop[n] = Xs[n, 1]
+            elseif xloop[n] > Xs[n, end]
+                xloop[n] = Xs[n, end]
             end
         end
-        it+=1;
+        it += 1
     end
-    println(it)
-    return xloop;
+    return xloop
+end
+
+
+function Broyden_secant(f, J, xstart, Xs, tol = 1e-8, maxiter = 10000)
+    Jnew = J;
+
+    dd = Inf
+    it = 0
+    ndim = length(xstart)
+    xold = copy(xstart)
+    #making sure starting x is in support
+    for n = 1:ndim
+        @assert ((xold[n] >= Xs[n, 1]) && (xold[n] <= Xs[n, end])) "Starting value is not in function support Xs"
+    end
+    while (sum(abs.(f(xold))) > tol) && (it < maxiter)
+        xnew = xold - inv(Jnew)*f(xold);
+        for n = 1:ndim
+            if xnew[n] < Xs[n, 1]
+                xnew[n] = Xs[n, 1]
+            elseif xnew[n] > Xs[n, end]
+                xnew[n] = Xs[n, end]
+            end
+        end
+        w = xnew-xold;
+        Jnew = Jnew*(((f(xnew)-f(xold))-Jnew*w)*w')/(w'*w);
+        xold = copy(xnew);
+        it += 1
+    end
+    return xnew
 end

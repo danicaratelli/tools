235666cd5f133442fa66e989df3b30679f3281d1
danicaratelli <danicaratelli@gmail.com>
2019-12-03 21:56:36 -0800

    simulation for Krussel Smith (98) done

--- Numerical Macro/KS/helper.jl
+++ Numerical Macro/KS/helper.jl
@@ -4,6 +4,9 @@
 ## employment status. For details on how to construct the transition matrix see
 ## Heer-Maussner Ch. 8.3
 
+ug = 0.0386;    #probability of unemployment in good state
+ub = 0.1073;    #probability of unemployment in bad state
+us = [ug;ub];
 ΓZ = [0.8 0.2; 0.2 0.8];    #transition matrix for aggregate state
 Γgg = [0.9615 0.0385; 0.9581 0.0419];
 Γbb = [0.9525 0.0475; 0.3952 0.6048];
@@ -11,14 +14,12 @@
 Γbg = [1 0; (ub-ug)/ub ug/ub];
 Γ = [Γgg*0.8 Γgb*0.2; Γbg*0.2 Γbb*0.8];
 
-## Grids
-    #asset holdings
+# constructing grids
 agrid = collect(range(amin,stop=amax,length=na));
 Kgrid = collect(range(Kmin,stop=Kmax,length=nk));
 
 
-## Household
-    #useful functions
+#useful functions
 N(Z) = floor.(Z)*(1-ug) .+ (1 .- floor.(Z))*(1-ub);
 u(Z) = 1 .- N(Z);
 w(K,Z) = (1-α)*Z.*(K./N(Z)).^(α);
@@ -26,6 +27,7 @@ r(K,Z) = α*Z.*(N(Z)/K).^(1-α) .- δ;
 τ(K,Z) = 1 ./((w(K,Z).*N(Z) .+ r(K,Z)*K)./(ζ*w(K,Z).*u(Z)) .+ 1);
 
 
+# endogenous grid method for HH problem
 function solveHH(agrid,Kgrid,Zs,Hmat,iV=1,tol=1e-6,maxiter=1000)
     na = length(agrid);
     nK = length(Kgrid);
@@ -111,18 +113,141 @@ function solveHH(agrid,Kgrid,Zs,Hmat,iV=1,tol=1e-6,maxiter=1000)
     end
 end
 
-
+# linear interpolation routine (for singleton or vector)
 function interpolateK(k,Kgrid)
     nK = length(Kgrid);
-    if k<Kgrid[1]
-        kloc_min = 1;
-        w = 1;
-    elseif k>Kgrid[end]
-        kloc_min = nK-1;
-        w = 0;
+    if length(k)==1
+            nK = length(Kgrid);
+        if k<Kgrid[1]
+            kloc_min = 1;
+            wk = 1;
+        elseif k>Kgrid[end]
+            kloc_min = nK-1;
+            wk = 0;
+        else
+            kloc_min = findlast(k.>=Kgrid);
+            wk = 1 - (k - Kgrid[kloc_min])/(Kgrid[kloc_min + 1] - Kgrid[kloc_min]);
+        end
     else
-        kloc_min = findlast(k.>=Kgrid);
-        w = 1 - (k - Kgrid[kloc_min])/(Kgrid[kloc_min + 1] - Kgrid[kloc_min]);
+        kloc_min = Int.(zeros(length(k)));
+        wk = zeros(length(k));
+            #below minimum
+        kloc_min[k.<Kgrid[1]] .= 1;
+        wk[k.<Kgrid[1]] .= 1;
+        kloc_min[k.>Kgrid[end]] .= nk - 1;
+        wk[k.>Kgrid[1]] .= 0;
+        kintra = (k.>=Kgrid[1]) .* (k.<=Kgrid[end]);
+        kloc_min[kintra] = map(x->findlast(x.>=Kgrid),k[kintra]);
+        wk[kintra] = 1 .- (k[kintra] .- Kgrid[kloc_min[kintra]])./(Kgrid[kloc_min[kintra] .+ 1] .- Kgrid[kloc_min[kintra]]);
     end
-    return kloc_min, w;
+    return kloc_min, wk;
+end
+
+
+# Plotting policy functions from the household problem
+function plot_policies(agrid,Kgrid,cpol)
+    #plotting consumption policy function for low and high capital and employed and unemployed
+    close()
+    figure()
+    #low capital
+    subplot(2,1,1)
+    plot(agrid,cpol[1,:,1],label=L"$\epsilon = e$")
+    plot(agrid,cpol[2,:,1],label=L"$\epsilon = u$")
+    title("K = "*string(Kgrid[1]))
+    legend()
+    xticks([])
+    #high capital
+    subplot(2,1,2)
+    plot(agrid,cpol[1,:,end],label=L"$\epsilon = e$")
+    plot(agrid,cpol[2,:,end],label=L"$\epsilon = u$")
+    title("K = "*string(Kgrid[end]))
+    subplots_adjust(hspace=0.5)
+    xlabel("assets")
+end
+
+
+## Simuation of aggregate states
+function simul_states(Nsimul)
+    simulZ = Int.(zeros(Nsimul));
+    simulZ[1] = 1;
+    rnd_nums = rand(Nsimul);
+    tmpTmat = cumsum(ΓZ,dims=2);
+    for i=2:Nsimul
+        simulZ[i] = findfirst(rnd_nums[i] .<= tmpTmat[simulZ[i-1],:]);
+    end
+    return simulZ;
+end
+
+function simul_employment(NN,Nsimul,simulZ)
+    #employment distribution over time and workers
+    dist_employment = Int.(zeros(NN,Nsimul));
+    dist_employment[1:Int(floor(ug*NN)),1] .= 1;    #initilization for employed/unemployed
+    rnd_nums = rand(NN,Nsimul);
+    #creating the overall
+    tmpTmats = zeros(2,2,4);
+    tmpTmats[:,:,1] = cumsum(Γgg,dims=2);
+    tmpTmats[:,:,2] = cumsum(Γbb,dims=2);
+    tmpTmats[:,:,3] = cumsum(Γgb,dims=2);
+    tmpTmats[:,:,4] = cumsum(Γbg,dims=2);
+    for i=2:Nsimul
+        if simulZ[i-1]==1 && simulZ[i]==1
+            tmpTmat = tmpTmats[:,:,1];
+        elseif simulZ[i-1]==2 && simulZ[i]==2
+            tmpTmat = tmpTmats[:,:,2];
+        elseif simulZ[i-1]==1 && simulZ[i]==2
+            tmpTmat = tmpTmats[:,:,3];
+        elseif simulZ[i-1]==2 && simulZ[i]==1
+            tmpTmat = tmpTmats[:,:,4];
+        end
+
+        tmats = tmpTmat[1 .+ dist_employment[:,i-1],:];
+        dist_employment[:,i] = map(x->findfirst(rnd_nums[x,i].<tmats[x,:]),1:NN) .- 1;
+    end
+    return dist_employment;
+end
+
+
+
+function simulate_HH(NN,Nsimul,astart,simulZ,dist_employment,cpol,Kgrid,agrid)
+    #simulating using the HH policies to get aggregate capital in each period
+    Kpath = zeros(Nsimul);
+    asset_sim = zeros(NN,Nsimul);
+    asset_sim[:,1] .= astart;
+
+
+    for i=2:Nsimul
+        Klast = mean(asset_sim[:,i-1]);
+        Kpath[i-1] = Klast;
+        locK, wK = interpolateK(Klast,Kgrid);
+        locas, was = interpolateK(asset_sim[:,i-1],agrid);
+        ind_extract = zeros(Nsimul)
+        x_indxs = 2*(simulZ[i-1]-1) .+ 1 .+ dist_employment[:,i-1];
+
+        #low assets and low capital
+        C_alow_Klow = dist_employment[:,i-1].*cpol[2*(simulZ[i-1]-1)+2,locas,locK] .+ (1 .- dist_employment[:,i-1]).*cpol[2*(simulZ[i-1]-1)+1,locas,locK];
+        A_alow_Klow_e = (1 + (1-τ(Kgrid[locK],Zs[simulZ[i-1]]))*r(Kgrid[locK],Zs[simulZ[i-1]]))*agrid[locas] .+ (1-τ(Kgrid[locK],Zs[simulZ[i-1]]))*w(Kgrid[locK],Zs[simulZ[i-1]]) .- C_alow_Klow;
+        A_alow_Klow_u = (1 + (1-τ(Kgrid[locK],Zs[simulZ[i-1]]))*r(Kgrid[locK],Zs[simulZ[i-1]]))*agrid[locas] .+ ζ*(1-τ(Kgrid[locK],Zs[simulZ[i-1]]))*w(Kgrid[locK],Zs[simulZ[i-1]]) .- C_alow_Klow;
+        A_alow_Klow = dist_employment[:,i-1].*A_alow_Klow_u .+ abs.(1 .- dist_employment[:,i-1]).*A_alow_Klow_e;    #asset distribution when high
+
+        #high assets and low capital
+        C_ahigh_Klow = dist_employment[:,i-1].*cpol[2*(simulZ[i-1]-1)+2,locas.+1,locK] .+ (1 .- dist_employment[:,i-1]).*cpol[2*(simulZ[i-1]-1)+1,locas.+1,locK];
+        A_ahigh_Klow_e = (1 + (1-τ(Kgrid[locK],Zs[simulZ[i-1]]))*r(Kgrid[locK],Zs[simulZ[i-1]]))*agrid[locas.+1] .+ (1-τ(Kgrid[locK],Zs[simulZ[i-1]]))*w(Kgrid[locK],Zs[simulZ[i-1]]) .- C_ahigh_Klow;
+        A_ahigh_Klow_u = (1 + (1-τ(Kgrid[locK],Zs[simulZ[i-1]]))*r(Kgrid[locK],Zs[simulZ[i-1]]))*agrid[locas.+1] .+ ζ*(1-τ(Kgrid[locK],Zs[simulZ[i-1]]))*w(Kgrid[locK],Zs[simulZ[i-1]]) .- C_ahigh_Klow;
+        A_ahigh_Klow = dist_employment[:,i-1].*A_ahigh_Klow_u .+ abs.(1 .- dist_employment[:,i-1]).*A_ahigh_Klow_e;
+
+        #low assets and high capital
+        C_alow_Khigh = dist_employment[:,i-1].*cpol[2*(simulZ[i-1]-1)+2,locas,locK+1] .+ (1 .- dist_employment[:,i-1]).*cpol[2*(simulZ[i-1]-1)+1,locas,locK+1];
+        A_alow_Khigh_e = (1 + (1-τ(Kgrid[locK+1],Zs[simulZ[i-1]]))*r(Kgrid[locK+1],Zs[simulZ[i-1]]))*agrid[locas] .+ (1-τ(Kgrid[locK+1],Zs[simulZ[i-1]]))*w(Kgrid[locK+1],Zs[simulZ[i-1]]) .- C_alow_Khigh;
+        A_alow_Khigh_u = (1 + (1-τ(Kgrid[locK+1],Zs[simulZ[i-1]]))*r(Kgrid[locK+1],Zs[simulZ[i-1]]))*agrid[locas] .+ ζ*(1-τ(Kgrid[locK+1],Zs[simulZ[i-1]]))*w(Kgrid[locK+1],Zs[simulZ[i-1]]) .- C_alow_Khigh;
+        A_alow_Khigh = dist_employment[:,i-1].*A_alow_Khigh_u .+ abs.(1 .- dist_employment[:,i-1]).*A_alow_Khigh_e;
+
+        #high assets and high capital
+        C_ahigh_Khigh = dist_employment[:,i-1].*cpol[2*(simulZ[i-1]-1)+2,locas.+ 1,locK+1] .+ (1 .- dist_employment[:,i-1]).*cpol[2*(simulZ[i-1]-1)+1,locas.+ 1,locK+1];
+        A_ahigh_Khigh_e = (1 + (1-τ(Kgrid[locK+1],Zs[simulZ[i-1]]))*r(Kgrid[locK+1],Zs[simulZ[i-1]]))*agrid[locas.+1] .+ (1-τ(Kgrid[locK+1],Zs[simulZ[i-1]]))*w(Kgrid[locK+1],Zs[simulZ[i-1]]) .- C_ahigh_Khigh;
+        A_ahigh_Khigh_u = (1 + (1-τ(Kgrid[locK+1],Zs[simulZ[i-1]]))*r(Kgrid[locK+1],Zs[simulZ[i-1]]))*agrid[locas.+1] .+ ζ*(1-τ(Kgrid[locK+1],Zs[simulZ[i-1]]))*w(Kgrid[locK+1],Zs[simulZ[i-1]]) .- C_ahigh_Khigh;
+        A_ahigh_Khigh = dist_employment[:,i-1].*A_ahigh_Khigh_u .+ abs.(1 .- dist_employment[:,i-1]).*A_ahigh_Khigh_e;
+
+        asset_sim[:,i] = (was*wK).*A_alow_Klow .+ (was*(1-wK)).*A_alow_Khigh .+ ((1 .- was)*wK).*A_ahigh_Klow .+ ((1 .- was)*(1-wK)).*A_ahigh_Khigh;
+    end
+    return asset_sim, Kpath;
 end
--- Numerical Macro/KS/mainKS.jl
+++ Numerical Macro/KS/mainKS.jl
@@ -2,8 +2,7 @@
 ## description: mainKS.jl solves the basic Krussel-Smith (1998) model as in
 ## Heer-Maussner example 8.3
 
-using Dierckx #Interpolations
-
+using Dierckx, PyPlot #Interpolations
 
 ##  Parameters
 #grids
@@ -30,65 +29,29 @@ NN = 5000;   #number of households
 Nsimul = 3000;  #number of simulations
 Ngarbage = 500; #number of initial draws to throw out
 
+include("helper.jl")
 
 
 ## Step 1 (assuming t= 0 is good state)
 N0 = 0.95;
 K0 = ((1/β - δ)/α)^(1/(α-1))*N0;
 #computing the next period employment
-ug = 0.0386;    #probability of unemployment in good state
-ub = 0.1073;    #probability of unemployment in bad state
-us = [ug;ub];
 N1 = 1 .- us;
 
 ## Step 2-3
 Hmat = [0.2 0.9; 0.2 0.9];  #parameters for the law of motion of capital
 
 ## Step 4
-@time cpol, Vfun, tol_res, iter_res = solveHH(agrid,Kgrid,Zs,Hmat,1);
-
-#plotting consumption policy function for low and high capital and employed and unemployed
-close()
-figure()
-#low capital
-subplot(2,1,1)
-plot(agrid,cpol[1,:,1],label=L"$\epsilon = e$")
-plot(agrid,cpol[2,:,1],label=L"$\epsilon = u$")
-title("K = "*string(Kgrid[1]))
-legend()
-xticks([])
-#high capital
-subplot(2,1,2)
-plot(agrid,cpol[1,:,end],label=L"$\epsilon = e$")
-plot(agrid,cpol[2,:,end],label=L"$\epsilon = u$")
-title("K = "*string(Kgrid[end]))
-subplots_adjust(hspace=0.5)
-xlabel("assets")
+Zs = [Zg;Zb];
+@time cpol, tol_res, iter_res = solveHH(agrid,Kgrid,Zs,Hmat,0);
+plot_policies(agrid,Kgrid,cpol)
 
 
 ## Step 5
-# here we simulated the dynamics of the distribution function first by drawing a
-# series of states according to the transition function. We initialize everything
-# at the good state and cut off the first few observations
+#simulating aggregate states
+simulZ = simul_states(Nsimul);
+#simulating individual employment/unemployment states
+dist_employment = simul_employment(NN,Nsimul,simulZ);
 
-    #first getting simulations for aggregate state (starting from good state)
-simulZ = Int.(zeros(Nsimul));
-simulZ[1] = 1;
-rnd_nums = rand(Nsimul);
-tmpTmat = cumsum(ΓZ,dims=2);
-for i=2:Nsimul
-    simulZ[i] = findfirst(rnd_nums[i] .<= tmpTmat[simulZ[i-1],:]);
-end
-
-    #employment distribution over time and workers
-dist_employment = Int.(zeros(NN,Nsimul));
-dist_employment[1:Int(floor(ug*NN)),1] .= 1;    #initilization for employed/unemployed
-rnd_nums = rand(NN,Nsimul);
-tmpTmat = cumsum(Γ,dims=2);
-for i=2:Nsimul
-    tmats = tmpTmat[2*(simulZ[i-1])-1 .+ dist_employment[:,i-1],:];
-    tmats[i] = map(x->findfirst(rnd_nums[x,i].<tmats[x,:]),1:NN);
-    for ii=1:NN
-        dist_employment[ii,i] =
-    end
-end
+## Step 6
+@time A_sim, Kpath = simulate_HH(NN,Nsimul,5,simulZ,dist_employment,cpol,Kgrid,agrid);
